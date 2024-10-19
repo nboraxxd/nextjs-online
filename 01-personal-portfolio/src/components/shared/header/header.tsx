7110fcb310fe348530d1320cd8add11bdd41e748
nboraxxd <nhatbaoxxd@gmail.com>
2024-10-20 02:41:05 +0700

    feat: Add logo to header component

--- 01-personal-portfolio/src/components/shared/header/header.tsx
+++ 01-personal-portfolio/src/components/shared/header/header.tsx
@@ -1,15 +1,23 @@
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import Link from 'next/link'
+import { VercelLogoIcon } from '@radix-ui/react-icons'
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { Navbar } from '@/components/shared/header'
 import { HamburgerIcon } from '@/components/icons'
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-10 bg-background">
-      <div className="mx-auto flex h-header-height max-w-6xl items-center justify-end px-4 sm:px-16">
+      <div className="relative mx-auto flex h-header-height max-w-6xl items-center justify-center px-4 sm:justify-between sm:px-6 lg:px-16">
+        <Link
+          href="/"
+          className="inline-block p-1.5 transition-transform hover:scale-110 hover:bg-transparent active:scale-105 sm:p-2.5"
+        >
+          <VercelLogoIcon className="size-6 text-primary" />
+        </Link>
         <Navbar className="hidden sm:block" />
         <DropdownMenu>
-          <DropdownMenuTrigger className="p-2.5 sm:hidden">
+          <DropdownMenuTrigger className="absolute right-4 ml-auto p-2.5 sm:hidden">
             <HamburgerIcon className="h-[18px]" />
             <span className="sr-only">Toggle Navigation</span>
           </DropdownMenuTrigger>
